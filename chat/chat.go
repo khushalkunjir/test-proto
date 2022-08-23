bd702e8b1cde3b5cc2b5d4befcd2ff6c8fed6c0b
khushalkunjir <43520593+khushalkunjir@users.noreply.github.com>
2022-08-23 12:05:03 +0530

    Updated

--- chat/chat.go
+++ chat/chat.go
@@ -10,11 +10,13 @@ type Server struct {
 }
 
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
+	log.Printf(ctx)
 	log.Printf("Received message body from client: %s", message.Body)
 	return &Message{Body: "Hello From the Server!"}, nil
 }
 
 func (s *Server) SendParameters(ctx context.Context, instruction Instruction) (*Instruction, error) {
+	log.Printf(ctx)
 	log.Printf("Received Instruction Ip address from client: %s", instruction.IpAddress)
 	log.Printf("Received Instruction port from client: %s", instruction.Port)
 	log.Printf("Received Instruction name from client: %s", instruction.Name)
